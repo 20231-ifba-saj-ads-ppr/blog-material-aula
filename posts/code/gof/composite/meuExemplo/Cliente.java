3df5982339e8b2fde0fd5c5813950459da406acd
LuisADS8 <20211TADSSAJ0005@ifba.edu.br>
2023-06-01 18:24:51 -0300

    Atualização final

--- posts/code/gof/composite/meuExemplo/Cliente.java
+++ posts/code/gof/composite/meuExemplo/Cliente.java
@@ -10,7 +10,12 @@ public class Cliente {
         pacote1.adicionar(celular1, celular2, celular3);
 
         System.out.println(pacote1);
+        //Nome: Redmi Note 12 
+          //[Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100,
+          //Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100,
+          //Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100] - ID: RN1000
         System.out.println(pacote1.getPreco());
+        //3.513
         
         Component tablet1 = new Produto("Lenovo Tab P11 Plus", "Pacote: Lenovo,\nAno: 2021,\nRAM: 4GB,\nROM: 64GB\n", "LTPP100", 1.899);
         Component tablet2 = tablet1.clone();
@@ -20,13 +25,29 @@ public class Cliente {
         pacote2.adicionar(tablet1, tablet2, tablet3);
 
         System.out.println(pacote2);
+        //Nome: Tab P11 Plus 
+          //[Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100,
+          //Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100,
+          //Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100] - ID: LTPP1000
         System.out.println(pacote2.getPreco());
+        //5.697
 
         Component pacote3 = new Pacote("Redmi Note 12 || Tab P11 Plus", "Pacote com 2 pacotes, um com celulares e outro com tablets, além de Redmi Note 12 indívidual", "RNLTPP100");
         Component celular4 = celular1.clone();
         pacote3.adicionar(pacote1, pacote2, celular4);
 
         System.out.println(pacote3);
+        //Nome: Redmi Note 12 || Tab P11 Plus 
+          //[Nome: Redmi Note 12 
+            //[Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100, 
+            //Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100,
+            //Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100] - ID: RN1000,
+          //Nome: Tab P11 Plus 
+            //[Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100,
+            //Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100,
+            //Nome: Lenovo Tab P11 Plus - Preço: 1.899 - ID: LTPP100] - ID: LTPP1000,
+          //Nome: Xiaomi Redmi Note 12 - Preço: 1.171 - ID: RN100] - ID: RNLTPP100
         System.out.println(pacote3.getPreco());
+        //10.381
     }
 }
