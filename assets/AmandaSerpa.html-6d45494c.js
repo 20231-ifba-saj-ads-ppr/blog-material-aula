b1a1d890da87beafd507d688d61ffe2e83ea22e3
leandro-costa <leandro.costa@ifba.edu.br>
2023-06-29 23:57:22 +0000

    Deploying to gh-pages from @ 20231-ifba-saj-ads-ppr/blog-material-aula@1a925d369c5cdfc84da5d7a6e1d22b95a4e1614a 🚀

---
+++ assets/AmandaSerpa.html-6d45494c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as r,c as a}from"./app-089705e0.js";const t={};function c(o,n){return r(),a("div")}const s=e(t,[["render",c],["__file","AmandaSerpa.html.vue"]]);export{s as default};
