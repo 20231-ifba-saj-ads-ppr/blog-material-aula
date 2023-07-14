eac5e5318ec85e5a999684107ad51e6cf89db3d1
leandro-costa <leandro.costa@ifba.edu.br>
2023-07-14 00:27:15 +0000

    Deploying to gh-pages from @ 20231-ifba-saj-ads-ppr/blog-material-aula@731d9bed2906c613509630353d1d9858f9ed136a 🚀

---
+++ assets/SarahPithon.html-a9bcca7c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as o}from"./app-9e6bbe25.js";const r={};function c(a,n){return t(),o("div")}const f=e(r,[["render",c],["__file","SarahPithon.html.vue"]]);export{f as default};
